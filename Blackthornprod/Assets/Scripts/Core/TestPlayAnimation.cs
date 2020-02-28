4ef8340a681d020accdbc1a678baabf19597431a
Ron3 <luodm03@qq.com>
2020-02-28 17:09:15 +0800

    -change: 提交女武神的资源.能正常跑起来了.

--- Blackthornprod/Assets/Scripts/Core/TestPlayAnimation.cs
+++ Blackthornprod/Assets/Scripts/Core/TestPlayAnimation.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class TestPlayAnimation : MonoBehaviour
 {
+    private Animator animator;
+    private bool isShowLog = false;
 
     /// <summary>
     /// 
     /// </summary>
     private void Start() 
     {
-        Animator animator = this.gameObject.GetComponent<Animator>();
-        animator.SetBool("isRun", true);
+        animator = this.gameObject.GetComponent<Animator>();
+        // bool isRet1 = animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+        // bool isRet2 = animator.GetCurrentAnimatorStateInfo(0).IsName("character_run");
+        // Debug.Log($"isRet1 ==>  {isRet1}, isRet2 ==> {isRet2}");
+        // animator.SetBool("isRun", true);
     }
     
     /// <summary>
@@ -22,5 +27,23 @@ public class TestPlayAnimation : MonoBehaviour
         Debug.Log("onRonEvent!!");
         // Debug.Log($"onRonEvent.....{obj.name}");
     }
+
+    void Update()
+    {  
+        // AnimatorStateInfo info = animator.GetCurrentAnimatorStateInfo(0);    
+        // if (info.normalizedTime >= 1.0f && this.isShowLog == false)  
+        // {
+        //     Debug.Log($"播放完毕后的逻辑代码 ===> {info.normalizedTime}");
+        //     this.isShowLog = true;
+        //     animator.SetBool("isSub", true);
+        // }  
+        // else
+        // {
+        //     bool isRet1 = animator.GetCurrentAnimatorStateInfo(0).IsName("Idle");
+        //     bool isRet2 = animator.GetCurrentAnimatorStateInfo(0).IsName("character_run");
+        //     Debug.Log($"isRet1 ==>  {isRet1}, isRet2 ==> {isRet2}");
+        // }
+    }
+
 }
 
